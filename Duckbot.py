1168c451ee01b6cca1648faf96ad0cf11d412d7b
LingeringBlizzard <lingeringblizzard@gmail.com>
2022-01-12 16:20:52 -0600

    Fix for the mute command

--- Duckbot.py
+++ Duckbot.py
@@ -11,9 +11,9 @@ client = discord.Client()
 async def mute(ctx, member: discord.Member):
 	if ctx.message.author.guild_permissions.administrator == True or ctx.message.author.id == 592430350630912004:
 		role = discord.utils.get(member.server.roles, name='Muted')
-        await bot.add_roles(member, role)
-        embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)
-        await bot.say(embed=embed)
+		await bot.add_roles(member, role)
+		embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)
+		await bot.say(embed=embed)
 @bot.command(pass_context=True, brief='This will change a users nickname', help='This command will change the nickname of a user')
 async def nick(ctx, member: discord.Member, nick):
 	if ctx.message.author.guild_permissions.administrator == True or ctx.message.author.id == 592430350630912004:
