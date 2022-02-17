ab960ddc5aaf2372a67fdadc58a191e367385433
LingeringBlizzard <97249047+LingeringBlizzard@users.noreply.github.com>
2022-02-17 16:26:28 -0600

    Update Duckbot.py

--- Duckbot.py
+++ Duckbot.py
@@ -12,7 +12,7 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.members = True
-bot = commands.Bot(command_prefix="$", intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 file = ""
 client = discord.Client()
 
